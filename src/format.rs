5d8b3a2775e9f564b250afd85f5705b9dbb51ff8
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2021-02-24 13:36:42 +0100

    format: Add tests for Serato Timecode CD

--- src/format.rs
+++ src/format.rs
@@ -24,3 +24,63 @@ pub const SERATO_CONTROL_CD_1_0_0: TimecodeFormat = TimecodeFormat {
     taps: 0b0011_0100_1101_0101_0101,
     signal_frequency_hz: 1000.0,
 };
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::{Timecode, TimecodeAudioGenerator};
+
+    fn test_format(format: &TimecodeFormat, sample_rate_hz: f64) {
+        let mut generator = TimecodeAudioGenerator::new(format, sample_rate_hz);
+        let mut timecode = Timecode::new(format, sample_rate_hz);
+        let initial_state = generator.state();
+        let mut previous_timecode_state = timecode.state();
+        let mut state_changed = false;
+        assert_eq!(timecode.state(), initial_state);
+        assert_eq!(timecode.state(), generator.state());
+        println!(
+            "{:0>size$b} {:0>size$b}",
+            timecode.state(),
+            generator.state(),
+            size = format.size,
+        );
+
+        // Skip the first few samples until the bit detection works properly
+        for _ in 0..20 {
+            let (left, right) = generator.next_sample();
+            timecode.process_channels(left, right);
+        }
+        timecode.set_state(generator.state());
+
+        loop {
+            let (left, right) = generator.next_sample();
+            timecode.process_channels(left, right);
+            if timecode.state() != previous_timecode_state {
+                println!(
+                    "{:0>size$b} {:0>size$b}",
+                    timecode.state(),
+                    generator.state(),
+                    size = format.size,
+                );
+
+                assert_eq!(timecode.state(), generator.state());
+                previous_timecode_state = timecode.state();
+                state_changed = true;
+            }
+
+            if state_changed && generator.state() == initial_state {
+                break;
+            }
+        }
+    }
+
+    #[test]
+    fn test_serato_control_cd_1_0_0_44100hz() {
+        test_format(&SERATO_CONTROL_CD_1_0_0, 44100.0);
+    }
+
+    #[test]
+    fn test_serato_control_cd_1_0_0_48000hz() {
+        test_format(&SERATO_CONTROL_CD_1_0_0, 48000.0);
+    }
+}
