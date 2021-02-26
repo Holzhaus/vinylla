ce096fcbf11d8755dfa759f86018206f4fc28bc9
Holzhaus <Holzhaus@users.noreply.github.com>
2021-02-26 11:35:53 +0000

    deploy: 659fe1e9a9f50cab0b59b8ed7daaeccdf963ff47

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["vinylla"] = {"name":"","files":["bits.rs","bitstream.rs","format.rs","lfsr.rs","lib.rs","pitch.rs","timecode.rs","util.rs","visualizer.rs"]};
+sourcesIndex["vinylla"] = {"name":"","files":["bits.rs","bitstream.rs","format.rs","generator.rs","lfsr.rs","lib.rs","pitch.rs","timecode.rs","util.rs","visualizer.rs"]};
 createSourceSidebar();
