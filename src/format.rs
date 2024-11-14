6a43450bb237b63eb87a64fde431542424a29b8d
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2024-11-15 00:46:07 +0100

    chore: Silence some clippy warnings

--- src/format.rs
+++ src/format.rs
@@ -19,8 +19,8 @@ pub struct TimecodeFormat {
 pub const SERATO_CONTROL_CD_1_0_0: TimecodeFormat = TimecodeFormat {
     size: 20,
     seed: 0b1001_0001_0100_1010_1011,
-    /// LFSR feedback polynomial:
-    /// x^20 + x^18 + x^16 + x^14 + x^12 + x^10 + x^9 + x^6 + x^4 + x^3 + 1
+    // LFSR feedback polynomial:
+    // x^20 + x^18 + x^16 + x^14 + x^12 + x^10 + x^9 + x^6 + x^4 + x^3 + 1
     taps: 0b0011_0100_1101_0101_0101,
     signal_frequency_hz: 1000.0,
 };
