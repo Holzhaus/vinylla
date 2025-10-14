9d620e32f1d990d449411b711b980536a0a075db
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2025-10-14 23:54:44 +0200

    chore: Update copyright headers for 2025

--- examples/serato.rs
+++ examples/serato.rs
@@ -1,3 +1,11 @@
+// Copyright (c) 2025 Jan Holthuis <jan.holthuis@rub.de>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy
+// of the MPL was not distributed with this file, You can obtain one at
+// http://mozilla.org/MPL/2.0/.
+//
+// SPDX-License-Identifier: MPL-2.0
+
 //! Reads a Serato Control CD 1.0.0 WAV file and prints the decoded positions.
 //!
 //! The WAV file can be downloaded from:
