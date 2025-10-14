64ae6db842770e94e25137a22dea822f2e2b4b39
Holzhaus <Holzhaus@users.noreply.github.com>
2025-10-14 22:14:16 +0000

    deploy: c29d284bd03d151ea251217c57bcacc4ccb54a51

--- src-files.js
+++ src-files.js
@@ -1,3 +1,2 @@
-var srcIndex = new Map(JSON.parse('[["vinylla",["",[],["bits.rs","bitstream.rs","format.rs","generator.rs","lfsr.rs","lib.rs","pitch.rs","timecode.rs","util.rs","visualizer.rs"]]]]'));
-createSrcSidebar();
-//{"start":36,"fragment_lengths":[143]}
+createSrcSidebar('[["vinylla",["",[],["bits.rs","bitstream.rs","format.rs","generator.rs","lfsr.rs","lib.rs","pitch.rs","timecode.rs","util.rs","visualizer.rs"]]]]');
+//{"start":19,"fragment_lengths":[143]}
